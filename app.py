65317d4b4c898ded11f9ecc4dc41a57020bc7f36
Vi Nguyen <xuanvidz03102003@gmail.com>
2026-04-10 16:40:58 +0700

    fix

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ from ultralytics import YOLO
 model = YOLO("yolov10l.pt")
 
 # Export ONNX với imgsz=640
-model.export(format="onnx", imgsz=640, opset=13, simplify=True)
+model.export(format="onnx",opset=13, simplify=True)
 
 print("✅ Export ONNX thành công (imgsz=640)")
