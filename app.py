eafc437f3d35e18d4ca4b0ff49878236a4cad7a0
Vi Nguyen <xuanvidz03102003@gmail.com>
2026-04-10 16:33:35 +0700

    Add project files.

---
+++ app.py
@@ -0,0 +1,9 @@
+from ultralytics import YOLO
+
+# Load model
+model = YOLO("yolov10l.pt")
+
+# Export ONNX với imgsz=640
+model.export(format="onnx", imgsz=640, opset=13, simplify=True)
+
+print("✅ Export ONNX thành công (imgsz=640)")
